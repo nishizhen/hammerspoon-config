17e6de306456eff3f276268d956382f0e602c920
dujianpeng <nishizhen@gmail.com>
2022-11-25 11:37:36 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# hammerspoon-config is my configuration for configuration [Hammerspoon](http://www.hammerspoon.org/). 
+ 
+ It provides some functionality like desktop widgets, window management, bing wallpaper ... etc.
+
+## Get started
+
+1. Install [Hammerspoon](http://www.hammerspoon.org/) first.
+
+2. `git clone https://github.com/nishizhen/hammerspoon-config.git ~/.hammerspoon`
+
+3. Reload the configutation.
+
+## Keep update
+
+`cd ~/.hammerspoon && git pull`
---
+++ init.lua
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------------------------
+-- Register hammerspoon Reload config file
+hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
+if string.len(hsreload_keys[2]) > 0 then
+    hs.hotkey.bind(hsreload_keys[1], hsreload_keys[2], "Reload Configuration", function() hs.reload() end)
+end
+----------------------------------------------------------------------------------------------------
+-- Define default Spoons which will be loaded later
+if not hspoon_list then
+    hspoon_list = {
+        "BingDaily",
+        "HCalendar",
+        -- "WinWin",
+        -- "FnMate",
+    }
+end
+-- Load those Spoons
+for _, v in pairs(hspoon_list) do
+    hs.loadSpoon(v)
+end
+----------------------------------------------------------------------------------------------------
+-- Register windowHints
+hs.hotkey.bind({"alt"}, "tab", function()
+    hs.hints.windowHints()
+end)
+----------------------------------------------------------------------------------------------------
+-- Register lock screen
+hs.hotkey.bind({"cmd"}, "L", function()
+    hs.caffeinate.lockScreen()
+end)
