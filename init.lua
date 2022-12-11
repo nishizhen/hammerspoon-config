be805ebec58146e66f20abb9b28e910c40af917a
dujianpeng <nishizhen@gmail.com>
2022-12-11 14:28:57 +0800

    change the code order, no feature changed

--- init.lua
+++ init.lua
@@ -1,25 +1,4 @@
-----------------------------------------------------------------------------------------------------
--- Auto Start brew bundle backup
--- hs.hotkey.bind({"cmd", "ctrl"}, "T", function()
-local autoDumpBrewBundleTimer = hs.timer.doEvery(6 * 60 * 60, function()
-    local brewFilePath, brewFileName = "~/Documents/", "Brewfile"
-    local notifyTitle = "Auto dump Brew bundle result"
-    local result = hs.execute("cd " .. brewFilePath .. " && brew bundle dump -f", true)
-    local BrewfileModTime = hs.fs.attributes(brewFilePath .. brewFileName, "modification")
-    local diff = os.time() - BrewfileModTime
-    if diff < 5 then
-        if diff >= 0 then
-            hs.notify.show(notifyTitle, "Success!", "Brewfile path: " .. brewFilePath .. brewFileName)
-        else
-            hs.notify.show(notifyTitle, "Error!!!!!", "")
-        end
-    else
-        hs.notify.show(notifyTitle, "Error!!!!!", "")
-    end
-end)
-autoDumpBrewBundleTimer:setNextTrigger(5)
-autoDumpBrewBundleTimer:start()
--- end)
+
 ----------------------------------------------------------------------------------------------------
 -- Register hammerspoon Reload config file
 hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
@@ -28,6 +7,28 @@ if string.len(hsreload_keys[2]) > 0 then
         hs.reload()
     end)
 end
+----------------------------------------------------------------------------------------------------
+-- Auto Start brew bundle backup
+-- hs.hotkey.bind({"cmd", "ctrl"}, "T", function()
+    local autoDumpBrewBundleTimer = hs.timer.doEvery(6 * 60 * 60, function()
+        local brewFilePath, brewFileName = "~/Documents/", "Brewfile"
+        local notifyTitle = "Auto dump Brew bundle result"
+        local result = hs.execute("cd " .. brewFilePath .. " && brew bundle dump -f", true)
+        local BrewfileModTime = hs.fs.attributes(brewFilePath .. brewFileName, "modification")
+        local diff = os.time() - BrewfileModTime
+        if diff < 5 then
+            if diff >= 0 then
+                hs.notify.show(notifyTitle, "Success!", "Brewfile path: " .. brewFilePath .. brewFileName)
+            else
+                hs.notify.show(notifyTitle, "Error!!!!!", "")
+            end
+        else
+            hs.notify.show(notifyTitle, "Error!!!!!", "")
+        end
+    end)
+    autoDumpBrewBundleTimer:setNextTrigger(5)
+    autoDumpBrewBundleTimer:start()
+    -- end)
 ----------------------------------------------------------------------------------------------------
 -- Define default Spoons which will be loaded later
 if not hspoon_list then
